b21bb7a611579f7a94abff87b3c5957ffe970ba4
WIDEROZ <roftelio001@gmail.com>
2025-09-09 12:12:41 +0200

    obsidian-auto-push : 2025-09-09 12:12:41

--- Informatique/Operation_sur_les_processus.c
+++ Informatique/Operation_sur_les_processus.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
 #include <unistd.h>
-fork(void);
-pid_t res_fork ;
+
+int main(){
+    pid_t res_fork;
+    while (1){
+        res_fork = fork();
+
+        if (res_fork == 0){
+            printf("C'est moi le gosse\n");
+        }
+        else if(res_fork == -1){
+            perror("Création de processus");
+        }
+        else{
+            printf("Je suis ton père, mais mon fils tu t'appelles : %d \n", res_fork);
+        }
+    }
+
+    return 0;
+}
+
