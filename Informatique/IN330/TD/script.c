9535e3a159299ce8b83d63575f5486887f2680cc
WIDEROZ <roftelio001@gmail.com>
2025-09-12 14:00:33 +0200

    obsidian-auto-push : 2025-09-12 14:00:33

--- Informatique/IN330/TD/script.c
+++ Informatique/IN330/TD/script.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+float x0;
+scanf("Saisir la valeur pour calculer le polynome : %f", &x0);
+
+float puissance
+
+
+float valeurPolynome(float P[], int taille, int val){
+    float tmp = 0;
+    for(int i=0; i < taille; i++){
+        tmp += ;
+    }
+    return ;
+}
+
 int main(){
-    
+    float P[4] P = {2; 1; 0; 4};
+
+    printf("La valeur du polynome est : %f", valeurPolynome(P, 4, x0));
+
+
     return 0;
 }
