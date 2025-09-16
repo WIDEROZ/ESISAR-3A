3ec3053f2635942e285ba87e851dcf7888cce8a5
WIDEROZ <roftelio001@gmail.com>
2025-09-16 16:34:48 +0200

    obsidian-auto-push : 2025-09-16 16:34:48

--- Informatique/IN332/TD/Exercice 1.c
+++ Informatique/IN332/TD/Exercice 1.c
@@ -13,15 +13,23 @@ int main(){
         exit(2);
     }
     else if(proc1 == 0){
-        printf("Le PID du fils est : %d, PID du père : %d \n", getpid(), getppid());
+        printf("Le PID du fils 1 est : %d, et celui du père est : %d \n", getpid(), getppid());
         sleep(2);
         exit(0);
     }
     else{
-        printf("Père du fils PID : %d \n", getpid());
-        wait(NULL);
-        printf("Reveil du père \n");
-        exit(0);
+        if(fork() == 0){
+            printf("Le PID du fils 2 est : %d, et celui du père est : %d \n", getpid(), getppid());
+            sleep(5);
+            exit(0);
+        }
+        else{
+            printf("Père du fils 2 PID : %d \n", getpid());
+            wait(NULL);
+            printf("Reveil du père \n");
+            exit(0);
+        }
+        
     }
 
 
--- Informatique/IN332/TD/Exercice 2.c
+++ Informatique/IN332/TD/Exercice 2.c
@@ -7,8 +7,21 @@
 int main(){
     int n = 100 ;
     printf(" bonjour --> ");
-    n *= 2 ;
-    printf("ZJKFHU %d \n", n);
+    n *= 2;
+    if (fork() == 0){
+        sleep(1);
+        printf(" dans le fils, adresse de n = %p\n ", &n) ;
+        n += 10 ;
+        sleep(1) ;
+        printf(« n = %d\n », n) ;
+    }
+    else {
+        printf(« dans le pere, adresse de n = %p\n », &n);
+        n += 20 ;
+        sleep(3) ;
+        printf(« n = %d\n », n) ;
+    }
+    exit(0) ;
     
     
     
