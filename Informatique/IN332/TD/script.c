5e27bb376bb8ef9f084128932c33fe07ab60cbc4
WIDEROZ <roftelio001@gmail.com>
2025-09-16 16:14:47 +0200

    obsidian-auto-push : 2025-09-16 16:14:47

--- Informatique/IN332/TD/script.c
+++ Informatique/IN332/TD/script.c
@@ -5,39 +5,26 @@
 #include <stdlib.h>
 
 int main(){
-    pid_t proc1, proc2;
-    int *stat1, *stat2;
+    int *status;
+    pid_t proc1;
     proc1 = fork();
-    proc2 = fork();
 
     if (proc1 == -1){
         printf("Erreur dans la création du processus 1 \n");
+        exit(2);
     }
     else if(proc1 == 0){
-        printf("Père du fils 1 \n");
+        printf("Père du fils 1 PID : %d \n", getppid());
+        waitpid(getpid(), status, 0);
+        printf("Reveil du père \n");
+        exit(0);
     }
     else{
         printf("Le PID du fils 1 est : %d \n", proc1);
+        sleep(2);
+        exit(0);
     }
 
-
-
-    if (proc2 == -1){
-        printf("Erreur dans la création du processus 2 \n");
-    }
-    else if(proc2 == 0){
-        printf("Père du fils 2 \n");
-    }
-    else{
-        printf("Le PID du fils 2 est : %d \n", proc2);
-    }
-
-    printf("Wait 1 : %d \n", waitpid(proc1, stat1, 0));
-    printf("Wait 2 : %d \n", waitpid(proc2, stat2, 0));
-
-
-    exit(0);
-    exit(0);
 
 
 }
