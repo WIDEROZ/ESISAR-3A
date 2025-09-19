5cdce6ff0c63d9a45864aded4872f0d4ca29bede
WIDEROZ <roftelio001@gmail.com>
2025-09-19 09:45:43 +0200

    obsidian-auto-push : 2025-09-19 09:45:43

--- Informatique/IN330/TD/TD2.c
+++ Informatique/IN330/TD/TD2.c
@@ -3,26 +3,38 @@
 
 void affichagerEnHexadecimal(char mot[]){
     char *l = mot;
-    for(;*l != '\0'; l = l+1){
+    for(;*l != '\0'; l++){
         printf("La valeure de la lettre %c est : %x \n", *l, *l);
     }
 }
 
 void affichagerEnDecimal(char mot[]){
     char *l = mot;
-    for(;*l != '\0'; l = l+1){
+    for(;*l != '\0'; l++){
         printf("La valeure de la lettre %c est : %d \n", *l, *l);
     }
 }
 
 int bin_to_dec(char bin[]){
-    
+    char *str = bin;
+    int dec = 0;
+    for(; *str != '\0'; str++){
+        if(*str == '0'){
+            dec = dec*2;
+        }
+        else{
+            dec = dec*2+1;
+        }
+    }
+    return dec;
 }
 
 int main(){
     char * str = "LMAFO";
+    char * bin = "0110";
     affichagerEnHexadecimal(str);
     affichagerEnDecimal(str);
+    printf("AIE : %d \n", bin_to_dec(bin));
 
 
     return 0;
