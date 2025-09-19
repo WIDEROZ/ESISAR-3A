0f6e37fa2d71b7896760e76718d147a76e3558c2
WIDEROZ <roftelio001@gmail.com>
2025-09-19 15:00:13 +0200

    obsidian-auto-push : 2025-09-19 15:00:13

--- Informatique/IN330/TD/TD2 - EX2.c
+++ Informatique/IN330/TD/TD2 - EX2.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <types.h>
 
+int64_t codage_num(int s, int y, int m, int d, int c, int o){
+    int64_t num_secu = 0;
+        num_secu = s - int('1');
+        num_secu >> 
 
+
+
+    return num_secu;
+}
 
 
 
 int main(){
-    int64_t num;
+    int64_t num_secu;
+
 
 
     return 0;
