918b2a537d0c832c0708e7c9b4139fe6102f0f2f
WIDEROZ <roftelio001@gmail.com>
2025-09-19 09:15:42 +0200

    obsidian-auto-push : 2025-09-19 09:15:42

--- Informatique/IN330/TD/TD2.c
+++ Informatique/IN330/TD/TD2.c
@@ -3,15 +3,16 @@
 
 void affichagerEnHexadecimal(char mot[]){
     char l = mot[0];
-    for(,l == 0, (&l ++)){
-        printf("La valeure de la lettre %c est : %x", l, l);
+    for(;l != '\0'; l = *(&l+1)){
+        printf("La valeure de la lettre %c est : %x \n", l, *(&l+1));
     }
+    printf("test %c et %c", l, *(&l+1));
 }
 
 
 int main(){
+    char * str = "TEST";
 
-
-
+    affichagerEnHexadecimal(str);
     return 0;
 }
