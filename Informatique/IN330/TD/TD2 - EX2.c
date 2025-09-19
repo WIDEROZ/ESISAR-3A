eace3bbb56ce4b3863b3d13c231db702e1178f13
WIDEROZ <roftelio001@gmail.com>
2025-09-19 15:10:14 +0200

    obsidian-auto-push : 2025-09-19 15:10:14

--- Informatique/IN330/TD/TD2 - EX2.c
+++ Informatique/IN330/TD/TD2 - EX2.c
@@ -3,12 +3,31 @@
 
 int64_t codage_num(int s, int y, int m, int d, int c, int o){
     int64_t num_secu = 0;
-        num_secu = s - int('1');
-        num_secu >> 
+    
+    num_secu = s-1;
+    num_secu = num_secu << 7;
 
-
-
+    num_secu += y;
+    
+    
+    num_secu += m;
+    num_secu >> 4;
+    
+    num_secu += d;
+    num_secu >> 7;
+    
+    num_secu += c;
+    num_secu >> 10;
+    
+    num_secu += o;
+    num_secu >> 10;
+    
     return num_secu;
+}
+
+int* decodage_num(int64_t ss){
+    
+
 }
 
 
