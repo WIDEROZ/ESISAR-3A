1a162a6741cbb7d2607e9a9e815ac9794c0e38d6
WIDEROZ <roftelio001@gmail.com>
2025-10-10 14:58:16 +0200

    obsidian-auto-push : 2025-10-10 14:58:16

--- Informatique/IN330/TD/TD3/TD3 - Ex2.c
+++ Informatique/IN330/TD/TD3/TD3 - Ex2.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int estPalindrome(char* mot, int n){
     if(n < 0){
         return 1;
     }
-    else{
-        if(mot[0] == mot[n-1]){
-            mot++;
-            return estPalindrome(mot, n-2);
-        }
-        return 0;
+    else if(mot[0] == mot[n-1]){
+        return estPalindrome(mot+1, n-2);
     }
+    return 0;
 
 }
 
 
 int main(){
-    char* mot = "azertyuiopoiuytreza";
-    printf("Palindrome ? : %d \n", estPalindrome(mot, 18));
+    char* mot = "azertyuioppoiuytreza";
+    printf("Palindrome ? : %d \n", estPalindrome(mot, 20));
 
     
 
---
+++ Informatique/IN330/TD/TD3/TD3 - Ex3.md
@@ -0,0 +1,10 @@
+n = "allella"
+p = "allez_les_bleus"
+
+#### f1
+Sert à trouver un caractère dans une chaîne
+renvoie : 
+- $1$ si il l'a trouvé
+- $0$ sinon
+
+#### f2
