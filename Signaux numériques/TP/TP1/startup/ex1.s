be0bdcd379d886aac6327e8d66c482ae0513e634
WIDEROZ <roftelio001@gmail.com>
2025-11-10 19:34:43 +0100

    obsidian-auto-push : 2025-11-10 19:34:43

--- Signaux numériques/TP/TP1/startup/ex1.s
+++ Signaux numériques/TP/TP1/startup/ex1.s
@@ -1,41 +1,15 @@
 	.file	"ex1.c"
 	.option nopic
-	.attribute arch, "rv64i2p0_m2p0_a2p0_f2p0_d2p0_c2p0"
+	.attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0_zifencei2p0_zmmul1p0_zaamo1p0_zalrsc1p0_zca1p0_zcd1p0"
 	.attribute unaligned_access, 0
 	.attribute stack_align, 16
-# GNU C17 (GCC) version 8.3.0 (riscv64-unknown-elf)
-#	compiled by GNU C version 7.4.0, GMP version 6.1.2, MPFR version 4.0.1, MPC version 1.1.0, isl version none
-# warning: GMP header version 6.1.2 differs from library version 6.2.1.
-# warning: MPFR header version 4.0.1 differs from library version 4.2.0.
-# warning: MPC header version 1.1.0 differs from library version 1.3.1.
-# GGC heuristics: --param ggc-min-expand=30 --param ggc-min-heapsize=4096
-# options passed:  ex1.c -mtune=rocket -march=rv64imafdc -mabi=lp64d
-# -auxbase-strip ex1.s -fverbose-asm
-# options enabled:  -faggressive-loop-optimizations -fauto-inc-dec
-# -fchkp-check-incomplete-type -fchkp-check-read -fchkp-check-write
-# -fchkp-instrument-calls -fchkp-narrow-bounds -fchkp-optimize
-# -fchkp-store-bounds -fchkp-use-static-bounds
-# -fchkp-use-static-const-bounds -fchkp-use-wrappers -fcommon
-# -fdelete-null-pointer-checks -fdwarf2-cfi-asm -fearly-inlining
-# -feliminate-unused-debug-types -ffp-int-builtin-inexact -ffunction-cse
-# -fgcse-lm -fgnu-runtime -fgnu-unique -fident -finline-atomics
-# -fira-hoist-pressure -fira-share-save-slots -fira-share-spill-slots
-# -fivopts -fkeep-static-consts -fleading-underscore -flifetime-dse
-# -flto-odr-type-merging -fmath-errno -fmerge-debug-strings -fpeephole
-# -fplt -fprefetch-loop-arrays -freg-struct-return
-# -fsched-critical-path-heuristic -fsched-dep-count-heuristic
-# -fsched-group-heuristic -fsched-interblock -fsched-last-insn-heuristic
-# -fsched-rank-heuristic -fsched-spec -fsched-spec-insn-heuristic
-# -fsched-stalled-insns-dep -fschedule-fusion -fsemantic-interposition
-# -fshow-column -fshrink-wrap-separate -fsigned-zeros
-# -fsplit-ivs-in-unroller -fssa-backprop -fstdarg-opt
-# -fstrict-volatile-bitfields -fsync-libcalls -ftrapping-math -ftree-cselim
-# -ftree-forwprop -ftree-loop-if-convert -ftree-loop-im -ftree-loop-ivcanon
-# -ftree-loop-optimize -ftree-parallelize-loops= -ftree-phiprop
-# -ftree-reassoc -ftree-scev-cprop -funit-at-a-time -fverbose-asm
-# -fzero-initialized-in-bss -mdiv -mexplicit-relocs -mfdiv -mplt
-# -mriscv-attribute -mstrict-align
-
+# GNU C23 (g1b306039a) version 15.1.0 (riscv64-unknown-elf)
+#	compiled by GNU C version 11.4.0, GMP version 6.2.1, MPFR version 4.1.0, MPC version 1.2.1, isl version none
+# warning: GMP header version 6.2.1 differs from library version 6.3.0.
+# warning: MPFR header version 4.1.0 differs from library version 4.2.1.
+# warning: MPC header version 1.2.1 differs from library version 1.3.1.
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -mabi=lp64d -misa-spec=20191213 -march=rv64imafdc_zicsr_zifencei_zmmul_zaamo_zalrsc_zca_zcd
 	.text
 	.section	.rodata
 	.align	3
@@ -51,17 +25,17 @@ main:
 	sd	s0,16(sp)	#,
 	addi	s0,sp,32	#,,
 # ex1.c:7:   int x = 41;
-	li	a5,41		# tmp74,
-	sw	a5,-20(s0)	# tmp74, x
+	li	a5,41		# tmp136,
+	sw	a5,-20(s0)	# tmp136, x
 # ex1.c:8:   x = x+1;
-	lw	a5,-20(s0)		# tmp76, x
-	addiw	a5,a5,1	#, tmp75, tmp76
-	sw	a5,-20(s0)	# tmp75, x
+	lw	a5,-20(s0)		# tmp139, x
+	addiw	a5,a5,1	#, tmp140, tmp138
+	sw	a5,-20(s0)	# x_2, x
 # ex1.c:9:   printf("%d\n",x);
-	lw	a5,-20(s0)		# tmp77, x
-	mv	a1,a5	#, tmp77
-	lui	a5,%hi(.LC0)	# tmp78,
-	addi	a0,a5,%lo(.LC0)	#, tmp78,
+	lw	a5,-20(s0)		# tmp141, x
+	mv	a1,a5	#, tmp141
+	lui	a5,%hi(.LC0)	# tmp142,
+	addi	a0,a5,%lo(.LC0)	#, tmp142,
 	call	printf		#
 # ex1.c:11:   return 0;
 	li	a5,0		# _5,
@@ -72,4 +46,5 @@ main:
 	addi	sp,sp,32	#,,
 	jr	ra		#
 	.size	main, .-main
-	.ident	"GCC: (GNU) 8.3.0"
+	.ident	"GCC: (g1b306039a) 15.1.0"
+	.section	.note.GNU-stack,"",@progbits
