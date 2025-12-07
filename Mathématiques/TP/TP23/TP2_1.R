2dc492f569bc718050e30fb194133cfac9cc55e6
WIDEROZ <roftelio001@gmail.com>
2025-12-07 17:38:11 +0100

    obsidian-auto-push : 2025-12-07 17:38:11

--- Mathématiques/TP/TP23/TP2_1.R
+++ Mathématiques/TP/TP23/TP2_1.R
@@ -9,22 +9,22 @@ P[,5] <- c(  0,   5, 0.25, 0, 1, 0);
 P[,6] <- c(  0,   0, 0.25, 0, 0, 1);
 
 
-P2 = P %*% P;  
-t(pi0) %*% P2 %*% P2  
+#P2 = P %*% P;
+#t(pi0) %*% P2 %*% P2  
 
 liste_etat = 1:6;  
-N <- 1000;  
-count = 0;  
+N <- 100000;  
+count = 0;
 
-for (i in 1:N){  
-  Etat <- 2  
-  for (j in 1:4){  
+for (i in 1:N){
+  Etat <- 2;
+  for (j in 1:4){
     Etat <- sample(liste_etat, 1, prob = P[Etat, ]);  
-  }  
-  if(Etat == 2){  
-    count = count + 1;  
-  }  
-}  
+  }
+  if(Etat == 2){
+    count = count + 1;
+  }
+}
 
 count/N  
 count
