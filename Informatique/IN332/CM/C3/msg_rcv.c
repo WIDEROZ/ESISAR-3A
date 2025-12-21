643a128577f2effd5ca74b1e32e396465cd75299
WIDEROZ <roftelio001@gmail.com>
2025-12-21 17:45:01 +0100

    obsidian-auto-push : 2025-12-21 17:45:01

--- Informatique/IN332/CM/C3/msg_rcv.c
+++ Informatique/IN332/CM/C3/msg_rcv.c
@@ -18,17 +18,23 @@ int main(int argc, char const *argv[])
     }
     msg message;
     message.type = 1;
-    char content[256] = "Message test";
-    strcpy(content, message.content);
 
-    int id_file = msgget(key, IPC_CREAT | IPC_EXCL);
+    // Création de la file de messages
+    int id_file = msgget(key, 0666 | IPC_CREAT);
     if(id_file == -1){
         perror("Impossible de créer la file");
         exit(-1);
     }
 
+    // Recevoir un message
+    if(msgrcv(id_file, &message, sizeof(&message), (long)1, 0) == -1){
+        perror("Impossible de recevoir un message");
+        exit(-1);
+    }
 
 
+
+    // Supression de la file de messages
     if (msgctl(id_file, IPC_RMID, NULL) == -1) {
         perror("msgctl issue");
         return 1;
--- Informatique/IN332/CM/C3/msg_send.c
+++ Informatique/IN332/CM/C3/msg_send.c
@@ -1,2 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <string.h>
+#include <sys/msg.h>
+
+
+struct message{
+    long type;
+    char content[256];
+} typedef msg;
+
+
+int main(int argc, char const *argv[])
+{
+    int key = ftok("msg_rcv", 0);
+    if (key == -1){
+        perror("Impossible de créer la clé");
+        exit(-1);
+    }
+    msg message;
+    message.type = 1;
+    char content[256] = "Message test";
+    strcpy(content, message.content);
+
+    // Récupération de la file de messages
+    int id_file = msgget(key, IPC_CREAT);
+    if(id_file == -1){
+        perror("Impossible de créer la file");
+        exit(-1);
+    }
+    
+
+    return 0;
+}
