b76754106837a7bc56eed0f86aa5b669826a3528
WIDEROZ <roftelio001@gmail.com>
2025-12-21 17:35:00 +0100

    obsidian-auto-push : 2025-12-21 17:35:00

--- Informatique/IN332/CM/C3/msg_rcv.c
+++ Informatique/IN332/CM/C3/msg_rcv.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/ipc.h>
+#include <string.h>
+#include <sys/msg.h>
 
 struct message{
     long type;
@@ -9,16 +11,28 @@ struct message{
 
 int main(int argc, char const *argv[])
 {
-    int key = ftok("file_msg", 0);
+    int key = ftok("msg_rcv", 0);
     if (key == -1){
         perror("Impossible de créer la clé");
         exit(-1);
     }
     msg message;
     message.type = 1;
-    message.content = "Message test";
+    char content[256] = "Message test";
+    strcpy(content, message.content);
+
+    int id_file = msgget(key, IPC_CREAT | IPC_EXCL);
+    if(id_file == -1){
+        perror("Impossible de créer la file");
+        exit(-1);
+    }
 
 
+
+    if (msgctl(id_file, IPC_RMID, NULL) == -1) {
+        perror("msgctl issue");
+        return 1;
+    }
 
     return 0;
 }
