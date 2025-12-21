f4ff0f0f8aa9fb5ca790ed32b460524ba2bea0af
WIDEROZ <roftelio001@gmail.com>
2025-12-21 18:15:02 +0100

    obsidian-auto-push : 2025-12-21 18:15:02

--- Informatique/IN332/CM/C3/msg_snd.c
+++ Informatique/IN332/CM/C3/msg_snd.c
@@ -11,7 +11,9 @@ struct message{
     char content[256];
 } typedef msg;
 
+/* --------------- Envoie le nombre de messages qu'il faut pour bloquer la file --------------- */
 
+/*
 int main(int argc, char const *argv[])
 {
     int key = ftok("msg_rcv", 0);
@@ -31,11 +33,12 @@ int main(int argc, char const *argv[])
         exit(-1);
     }
 
-    
+    int count = 0;
     while(1){
         if(msgsnd(id_file, &message, sizeof(message.content), IPC_NOWAIT) == -1){
             if(errno == EAGAIN){
-                printf("La file est pleine");
+                printf("La file est pleine : %d\n", count);
+                break;
             }
             else{
                 perror("Problème lors de l'envoi du message");
@@ -43,6 +46,35 @@ int main(int argc, char const *argv[])
             }
             
         }
+        count ++;
+    }
+
+    return 0;
+}
+*/
+
+
+int main(int argc, char const *argv[]){
+    int key = ftok("msg_rcv", 0);
+    if (key == -1){
+        perror("Impossible de créer la clé");
+        exit(-1);
+    }
+    msg message;
+    message.type = 1;
+    char content[256] = "Message test";
+    strcpy(message.content, content);
+
+    // Récupération de la file de messages
+    int id_file = msgget(key, 0666 | IPC_CREAT);
+    if(id_file == -1){
+        perror("Impossible de récuérer la file");
+        exit(-1);
+    }
+
+    if(msgsnd(id_file, &message, sizeof(message.content), IPC_NOWAIT) == -1){
+        perror("Problème lors de l'envoi du message");
+        exit(-1);
     }
 
     return 0;
