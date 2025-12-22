ceb311ad3458bb482e59f8fa134ec74f44d32395
WIDEROZ <roftelio001@gmail.com>
2025-12-22 20:07:16 +0100

    obsidian-auto-push : 2025-12-22 20:07:16

--- Informatique/IN332/CM/C3/sem.c
+++ Informatique/IN332/CM/C3/sem.c
@@ -3,6 +3,7 @@
 #include <sys/sem.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/shm.h>
 
 #define SEM_NUM 1
 
@@ -30,10 +31,12 @@ void V(int sem_id){
 
 int main(int argc, char const *argv[])
 {
-    int *p1 = malloc(sizeof(int));
+    int shm_id = shmget(IPC_PRIVATE, sizeof(int), 0666 | IPC_CREAT);
+    int *p1 = (int*) shmat(shm_id, NULL, 0);
+    *p1 = 69;
+
     int *p2;
     struct sembuf tab_sem[SEM_NUM];
-    *p1 = 69;
 
     int key = ftok("sem", 0);
     int sem_id = semget(key, SEM_NUM, 0666 | IPC_CREAT);
@@ -57,12 +60,10 @@ int main(int argc, char const *argv[])
         printf("Process fils : %d\n", getpid());
 
         P(sem_id);
-        printf("fils 0 : p1 : %d\n", *p1);
         (*p1) ++;
-        sleep(2);
         printf("fils 1 : p1 : %d\n", *p1);
+        sleep(5);
         V(sem_id);
-
         printf("fils 2 : p1 : %d\n", *p1);
 
 
@@ -70,10 +71,10 @@ int main(int argc, char const *argv[])
     else{
         printf("Process père : %d\n", getpid());
 
+        sleep(1);
         P(sem_id);
         printf("père 0 : p1 : %d\n", *p1);
-        *p1 = 100;
-
+        *p1 = 0;
         printf("père 1 : p1 : %d\n", *p1);
         V(sem_id);
         printf("père 2 : p1 : %d\n", *p1);
