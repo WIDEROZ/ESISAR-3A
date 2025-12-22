2dac16cf1f1f4ea7769f8f9bbb89b117a1723739
WIDEROZ <roftelio001@gmail.com>
2025-12-22 19:37:15 +0100

    obsidian-auto-push : 2025-12-22 19:37:15

--- Informatique/IN332/CM/C3/sem.c
+++ Informatique/IN332/CM/C3/sem.c
@@ -5,12 +5,21 @@
 
 #define SEM_NUM 2
 
-int P(){
+void P(int sem_id, int i){
 
+    tab_sem[i].sem_op -= 1;
+    if(semop(sem_id, tab_sem, 1) == -1){
+        perror("semop issue\n");
+        exit(-1);
+    }
 }
 
-int V(){
-
+void V(int sem_id, int i){
+    tab_sem[i].sem_op += 1;
+    if(semop(sem_id, tab_sem, 1) == -1){
+        perror("semop issue\n");
+        exit(-1);
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -18,8 +27,9 @@ int main(int argc, char const *argv[])
     int *p1;
     int *p2;
     struct sembuf tab_sem[SEM_NUM];
+    
     *p1 = 69;
-
+    *p2 = 169;
 
     int key = ftok("sem", 0);
     int sem_id = semget(key, SEM_NUM, 0666 | IPC_CREAT);
@@ -37,10 +47,7 @@ int main(int argc, char const *argv[])
     else if(pid == 0){
         printf("Process fils : %d\n", getpid());
 
-        if(semop(sem_id, tab_sem, 1) == -1){
-            perror("semop issue\n");
-            exit(-1);
-        }
+        
 
 
 
