9288900ab2a59b324f4e3b152b7585f5fdd68268
WIDEROZ <roftelio001@gmail.com>
2025-12-27 16:27:30 +0100

    obsidian-auto-push : 2025-12-27 16:27:30

--- Signaux numériques/TD/TD3/exo2.s
+++ Signaux numériques/TD/TD3/exo2.s
@@ -1,24 +1,50 @@
     .text
     .globl main
 
+calculate:
+    addi sp, sp, -16
+    lw ra, 0(sp)
+
+    li t5, 42 # t5 : * : ascii
+    addi t6, t5, 1 # t6 : + : ascii
+
+    push:
+        lb t0, 0(a0)
+        beq t5, t0, operation # 
+
+        operation:
+
+
+        
+
+
+
+
+
+
+    sw ra, 0(sp)
+    addi sp, sp, 16
+    ret
+
+
+
 main:
     addi sp, sp, -16
     lw ra, 0(sp)
 
 
+    la a0, expression
+    la a1, res
+    call calculate
+
+
+    mv a0, a1
+    call println_int
 
 
 
 
-
-
-
-
-
-
-
-
-    sw
+    sw ra, 0(sp)
     addi sp, sp, 16
     ret
 
