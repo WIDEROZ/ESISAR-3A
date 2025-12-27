5493cf44dbe2bf88f55470f1703576368457a7d8
WIDEROZ <roftelio001@gmail.com>
2025-12-27 15:27:28 +0100

    obsidian-auto-push : 2025-12-27 15:27:28

--- Signaux numériques/TD/TD3/script.s
+++ Signaux numériques/TD/TD3/script.s
@@ -6,14 +6,19 @@ rev:
     sw ra, 0(sp)
 
     lw t0, 0(a0) # char chaine a retrouner
-    addi t1, zero, 4 # offset stack
-    sub sp, sp, t1
-    sw zero, 0(sp)
-    while:
+    addi sp, sp, -4
+    sb zero, 0(sp)
+    while1:
+        addi sp, sp, -1 # faire de la place pour la pile
+        addi a0, a0, 1  # char suivant
+        lb t0, 0(a0)    # char chaine a retrouner
+        sb t0, 0(sp)    # store dans la stack
 
-        
+        bne t0, zero, while1
+    
+    while2:
+        sw
 
-        bne t0, zero, while
 
 
 
