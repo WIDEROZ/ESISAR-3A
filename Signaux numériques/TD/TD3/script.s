ac9a17d36edf270a52ecb3d717a6ff3597a51fb3
WIDEROZ <roftelio001@gmail.com>
2025-12-27 15:07:26 +0100

    obsidian-auto-push : 2025-12-27 15:07:26

--- Signaux numériques/TD/TD3/script.s
+++ Signaux numériques/TD/TD3/script.s
@@ -1,28 +1,40 @@
     .text
     .globl main
 
-main:
+rev:
     addi sp, sp, -48
     sw ra, 0(sp)
+
+    lw t0, 0(a0)
+    while:
+        
+
+
+
+    lw ra, 0(sp)
+    addi sp, sp, 48
+    ret
+
+
+main:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw a0, 4(sp)
+    sw a1, 8(sp) 
 
 
     la a0, .machine
     la a1, .chaineresu
     call rev
 
+    mv a0, a1
+    call println_string
 
 
-
-
-
-
-
-
-
-
-
+    lw a1, 8(sp)
+    lw a0, 4(sp)
     lw ra, 0(sp)
-    addi sp, sp, 48
+    addi sp, sp, 16
     ret
 
     
