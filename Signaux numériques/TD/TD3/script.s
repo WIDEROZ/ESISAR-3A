4f2502068cdf95bb1338fd7c19d8e6bcbd7c4e8f
WIDEROZ <roftelio001@gmail.com>
2025-12-27 15:17:26 +0100

    obsidian-auto-push : 2025-12-27 15:17:26

--- Signaux numériques/TD/TD3/script.s
+++ Signaux numériques/TD/TD3/script.s
@@ -2,17 +2,23 @@
     .globl main
 
 rev:
-    addi sp, sp, -48
+    addi sp, sp, -16
     sw ra, 0(sp)
 
-    lw t0, 0(a0)
+    lw t0, 0(a0) # char chaine a retrouner
+    addi t1, zero, 4 # offset stack
+    sub sp, sp, t1
+    sw zero, 0(sp)
     while:
+
         
+
+        bne t0, zero, while
 
 
 
     lw ra, 0(sp)
-    addi sp, sp, 48
+    addi sp, sp, 16
     ret
 
 
