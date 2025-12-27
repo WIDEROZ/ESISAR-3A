d84ae46a1809c6ade9d5790ec200a6ab11b0120e
WIDEROZ <roftelio001@gmail.com>
2025-12-27 16:17:29 +0100

    obsidian-auto-push : 2025-12-27 16:17:29

---
+++ Signaux numériques/TD/TD3/exo2.s
@@ -0,0 +1,33 @@
+    .text
+    .globl main
+
+main:
+    addi sp, sp, -16
+    lw ra, 0(sp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    sw
+    addi sp, sp, 16
+    ret
+
+
+
+
+
+    .section .data
+expression:
+    .string "*+124"
+res:
+    .space 4
