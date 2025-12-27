f692340c84826cecc341445486c49da5f95e3e3c
WIDEROZ <roftelio001@gmail.com>
2025-12-27 16:37:30 +0100

    obsidian-auto-push : 2025-12-27 16:37:30

--- Signaux numériques/TD/TD3/exo2.s
+++ Signaux numériques/TD/TD3/exo2.s
@@ -8,14 +8,35 @@ calculate:
     li t5, 42 # t5 : * : ascii
     addi t6, t5, 1 # t6 : + : ascii
 
+    
+
     push:
-        lb t0, 0(a0)
-        beq t5, t0, operation # 
+        lb t0, 0(a0)          # t0 : char
+        beq t5, t0, operation # t0 = *
+        beq t6, t0, operation # t0 = +
+
+        # Si t0 = Chiffre : on effectue l'opération
+        addi t0, t0, -48 # t0 devient un entier
+
+        # On vérifie si l'opération est une multiplication ou une addition
+        lb t1, 0(sp)
+        addi sp, sp, 16
+
+        beq t1, t6, addition
+        beq t1, t5, multiplication
+
+        addition: 
+            
+
+        multiplication: 
+            
 
         operation:
+            addi sp, sp, -16
+            sb t0, 0(sp)
 
 
-        
+
 
 
 
