92a238b359060175282106f4789cc1e1efbcb882
WIDEROZ <roftelio001@gmail.com>
2025-12-27 16:57:31 +0100

    obsidian-auto-push : 2025-12-27 16:57:31

--- Signaux numériques/TD/TD3/exo2.s
+++ Signaux numériques/TD/TD3/exo2.s
@@ -11,41 +11,49 @@ calculate:
     addi sp, sp, -16
     sb zero, 0(sp) # '\0' dans la sp
 
-    mv t3, zero  # t3 : 0 (résultat des opérations)
-
     push:
         lb t0, 0(a0)          # t0 : char
         beq t5, t0, operation # t0 = *
         beq t6, t0, operation # t0 = +
 
+        # Si t0 = Chiffre : on effectue l'opération
+        addi t0, t0, -48 # t0 devient un entier
+
+        # On load l'opération dans t2
+        lb t2, 0(sp)
+        addi sp, sp, 16
+
         chiffre:
-            # Si t0 = Chiffre : on effectue l'opération
-            addi t0, t0, -48 # t0 devient un entier
+            # On prend le prochain chiffre
+            addi a0, a0, 1
+            lb t1, 0(a0)
+            addi, t1, t1, -48 # t1 devient un entier
 
             # On vérifie si l'opération est une multiplication ou une addition
+            beq t2, t5, multiplication
+
+            # addition:
+                add t0, t0, t1
+                j fin_operation
+
+            multiplication:
+                mult t0, t0, t1
+            
+            fin_operation:
+
+            # On load l'opération dans t2
             lb t2, 0(sp)
             addi sp, sp, 16
 
-            beq t2, t6, addition
-            beq t2, t5, multiplication
-
-            addition: 
-                addi a0, a0, 1
-                lb t1, 0(a0)
-                addi, t1, t1, -48 # t1 devient un entier
-                add t3, t0, t1
-
-            multiplication: 
-                
-            
-            b
+            beq t2, zero, END # Si t2 = '\0' c'est fini
+            j chiffre         # Sinon on boucle
 
         operation:
             addi sp, sp, -16
             sb t0, 0(sp)
 
 
-
+    END:
 
 
 
